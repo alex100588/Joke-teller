4f7bb1cbf9021a7a1d146a9271f2da2aeff449c9
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-11-23 17:07:20 +0200

    Add files via upload

---
+++ Joke teller/script.js
@@ -0,0 +1,23 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// VoiceRSS Speech Function
+function tellMe(joke) {
+  const jokeString = joke.trim().replace(/ /g, '%20');
+  // VoiceRSS Speech Parameters
+  VoiceRSS.speech({
+    key: 'b2f228950ccb4ca691109346bd8b4308',
+    src: jokeString,
+    hl: 'en-us',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false,
+  });
+}
+
