9a017aa1d9ae7105e38250d40426573ecfced22d
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-11-24 16:29:40 +0200

    Update script.js

--- Joke teller/script.js
+++ Joke teller/script.js
@@ -6,18 +6,45 @@ function toggleButton() {
   button.disabled = !button.disabled;
 }
 
-// VoiceRSS Speech Function
-function tellMe(joke) {
-  const jokeString = joke.trim().replace(/ /g, '%20');
-  // VoiceRSS Speech Parameters
-  VoiceRSS.speech({
-    key: 'b2f228950ccb4ca691109346bd8b4308',
-    src: jokeString,
-    hl: 'en-us',
-    r: 0,
-    c: 'mp3',
-    f: '44khz_16bit_stereo',
-    ssml: false,
-  });
+
+const tellMe = (joke) =>{
+    console.log('tell me ', joke);
+    // VoiceRSS Speech 
+    VoiceRSS.speech({
+      key: 'b2f228950ccb4ca691109346bd8b4308',
+      src: joke,
+      hl: 'en-us',
+      r: 0,
+      c: 'mp3',
+      f: '44khz_16bit_stereo',
+      ssml: false,
+    });
 }
 
+// fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart')
+// .then(res => res.json())
+// .then(data => console.log(data))
+
+async function getJokes(){
+    let joke = ''
+    try {
+        const response = await fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart')
+        const data = await response.json()
+        if(data.setup){
+            joke = `hi, I am Alex and the joke is ${data.setup} ... ${data.delivery}`
+        }else{
+            joke = data.joke
+        }
+        
+    } catch (error) {
+        alert('Conectare la api nereusita' + error);
+    }
+    // console.log(joke);
+    // console.log(resp);
+    tellMe(joke)
+}
+
+
+button.addEventListener('click', ()=>{
+    getJokes()
+})
